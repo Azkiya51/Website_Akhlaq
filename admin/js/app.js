0a69732fe8683a0bc508d271a39ab51ecde8e169
Adi Karunia <maulanayoga180@gamil.com>
2026-06-02 19:51:49 +0700

    1

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,31 +1,21 @@
-async function loadComponent(id,file){
-
-  const response =
-  await fetch(file);
-
-  const html =
-  await response.text();
-
-  document.getElementById(id)
-  .innerHTML = html;
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
 }
 
-loadComponent(
-  "sidebar-container",
-  "partial/sidebar.html"
-);
+loadComponent("sidebar-container", "partial/sidebar.html");
+loadComponent("header-container", "partial/header.html");
+loadComponent("footer-container", "partial/footer.html");
 
-loadComponent(
-  "header-container",
-  "partial/header.html"
-);
+function loadPage(page) {
+  loadComponent("content-container", `page/${page}.html`);
+}
 
-loadComponent(
-  "content-container",
-  "page/dashboard.html"
-);
+loadPage("dashboard");
 
-loadComponent(
-  "footer-container",
-  "partial/footer.html"
-);
+function setActiveMenu(element) {
+  document.querySelectorAll(".menu-item").forEach((item) => item.classList.remove("active"));
+
+  element.classList.add("active");
+}
