2db7231ef887605ec6ccb0c847e9a96421b3c161
Adi Karunia <maulanayoga180@gamil.com>
2026-06-03 18:05:55 +0700

    rapihin tampilan

--- admin/js/app.js
+++ admin/js/app.js
@@ -9,14 +9,13 @@ loadComponent("header-container", "partial/header.html");
 loadComponent("footer-container", "partial/footer.html");
 
 function loadPage(page, title) {
+  loadComponent("content-container", `page/${page}.html`);
 
-    loadComponent("content-container", `page/${page}.html`);
+  const pageTitle = document.getElementById("page-title");
 
-    const pageTitle = document.getElementById("page-title");
-
-    if (pageTitle) {
-        pageTitle.textContent = title;
-    }
+  if (pageTitle) {
+    pageTitle.textContent = title;
+  }
 }
 
 function setActiveMenu(element) {
@@ -24,3 +23,49 @@ function setActiveMenu(element) {
 
   element.classList.add("active");
 }
+
+function updateDate() {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+
+  const tanggal = today.toLocaleDateString("id-ID", options);
+
+  const dateElement = document.getElementById("current-date");
+
+  if (dateElement) {
+    dateElement.textContent = tanggal;
+  }
+}
+
+setTimeout(updateDate, 500);
+
+function updateClock() {
+  const now = new Date();
+
+  const jam = String(now.getHours()).padStart(2, "0");
+  const menit = String(now.getMinutes()).padStart(2, "0");
+  const detik = String(now.getSeconds()).padStart(2, "0");
+
+  // Desktop: 15:25:30
+  const desktopTime = `${jam}:${menit}:${detik}`;
+
+  // Mobile: 15:25
+  const mobileTime = `${jam}:${menit}`;
+
+  const desktopClock = document.getElementById("live-time");
+
+  const mobileClock = document.getElementById("mobile-time");
+
+  if (desktopClock) desktopClock.textContent = desktopTime;
+
+  if (mobileClock) mobileClock.textContent = mobileTime;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
